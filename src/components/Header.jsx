636534833845d386b821bca02000fad2bf167fd8
efe <eefee1987@gmail.com>
2023-10-10 07:36:13 +0200

    fix tailwind installation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
 function Header() {
-  return <h1>Meetup</h1>;
+  return (
+    <div>
+      <h1 className='underline text-5xl text-center'>Meetup-App</h1>
+    </div>
+  );
 }
 
 export default Header;
